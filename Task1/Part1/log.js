1c4af7d31940274ca6d32944f8bcf8b2c02afa0c
Максим Михайлович Шландаков <shland0omax@gmail.com>
2016-08-10 21:18:04 +0300

    task1 added

---
+++ Task1/Part1/log.js
@@ -0,0 +1,13 @@
+for(var i = 0; i < data.length; i++){
+    var value;
+    if (data[i] === undefined){
+        value = "не определено";
+    }
+    else if (data[i] === null){
+        value = "не указано";
+    }
+    else {
+        value = data[i];
+    }
+    console.log("data[" + i + "] = " + value);
+}
