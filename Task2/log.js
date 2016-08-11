dd458f9b92c2386c9f03bbed27c90d991933d51b
Максим Михайлович Шландаков <shland0omax@gmail.com>
2016-08-11 18:57:33 +0300

    log.js simplified

--- Task2/log.js
+++ Task2/log.js
@@ -1,8 +1,11 @@
 sum = [0,0,0];                                      //array of sums
+var zero_func = function(){ 
+    return 0; 
+}
 for(var i = 0; i < 5; i++){
     for (var j = 0; j < 3; j++){
-        var func = data[i]["getCount"+(j+1)];
-        sum[j] += func && func.call(data[i]) || 0; //if field is undefined or count field is invalid adds 0
+        var func = data[i]["getCount"+(j+1)] || zero_func;
+        sum[j] += func.call(data[i]); //if field is undefined or count field is invalid adds 0
     }
 }
 for (var j = 0; j < 3; j++){
