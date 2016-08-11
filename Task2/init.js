ca43f715e227d66a5ff744eac79704b39366ce86
Максим Михайлович Шландаков <shland0omax@gmail.com>
2016-08-11 16:16:23 +0300

    Task 2 working version added

--- Task2/init.js
+++ Task2/init.js
@@ -1,14 +1,15 @@
-function initialize(typeNumber){
+function initialize(typeNumber){        //generates objects with specified number
     var obj = { 
         count: random(1,10)
     };
     obj["getCount" + typeNumber] = function (){
         return this.count;
     }
+    return obj;
 }
 
 
 data = [];
 for (var i = 0; i < 5; i++){
-    data[i] = initalize(random(1,3));
+    data[i] = initialize(random(1,3));
 }
--- Task2/log.js
+++ Task2/log.js
@@ -1,1 +1,10 @@
-//logger func will be there soon
+sum = [0,0,0];                                      //array of sums
+for(var i = 0; i < 5; i++){
+    for (var j = 0; j < 3; j++){
+        var func = data[i]["getCount"+(j+1)];
+        sum[j] += func && func.call(data[i]) || 0; //if field is undefined or count field is invalid adds 0
+    }
+}
+for (var j = 0; j < 3; j++){
+    console.log("count" + (j+1) + " = " + sum[j]);
+}
